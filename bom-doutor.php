30024a04946451398593299d49ce6d96e0646b29
Emerson Silva <emerson-ods@hotmail.com>
2023-02-01 10:18:51 -0300

    feat: new version

--- bom-doutor.php
+++ bom-doutor.php
@@ -4,7 +4,7 @@
  * Plugin Name: Bom Doutor
  * Plugin URI: https://github.com/lifedesenvolve/bom-doutor/
  * Description: Plugin dedicado ao site bom doutor.
- * Version: 1.0.18
+ * Version: 1.0.19
  * Author: Agencia Life 
  * Author URI: https://github.com/lifedesenvolve/
  */
